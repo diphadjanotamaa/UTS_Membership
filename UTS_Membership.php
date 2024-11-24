44b553e46815031b3b837162f2fc460753f91438
diphadjanotamaa <allendd0211@gmail.com>
2024-11-25 00:09:27 +0700

    ini adalah proses sinkronisasi file Membership

---
+++ UTS_Membership.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halamanMembership</title>
+</head>
+<body>    
+    ini adalah proses sinkronisasi file home
+</body>
+</html>
